6f38ab8fafe16a7975e1e95f291f264165c1c492
njericooper <njericooper@gmail.com>
2019-02-21 17:29:12 -0600

    yo

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,13 @@
 import React from "react"
+import { Link } from "gatsby"
 import Header from "../components/header"
 
 export default () => (
   <div style={{ color: `teal` }}>
-    <Header />
-    <h1>About Gatsby</h1>
+    <Link to="/contact/">Contact</Link> 
+    <Link to="/">Home</Link> 
+    <Header headerText="About Gatsby" /> 
+    <Header headerText="It's pretty cool" />
     <p>Such wow. Very React.</p>
   </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
-export default () => ( <div style={{ color: `teal`}}>
-
+export default () => ( 
+    <div style={{ color: `teal`}}>
+    <Link to="/contact/">Contact</Link> 
+    <Link to="/about/">About</Link>
+    <Header headerText="SUP, Mane?!" />
     <h1>Sup?! I'm your friendly neighborhood Coop!</h1>
     <p>What a world. That shit slap, don't it?</p>
     <img src="https://source.unsplash.com/random/1200x600" alt="" />
     
     </div>
-            )
+    )
