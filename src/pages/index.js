d04a839a4cbae160de65cf63f8acabe96605c55d
njericooper <njericooper@gmail.com>
2019-02-20 21:54:15 -0600

    First commit

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default () => ( <div style={{ color: `teal`}}>
+
+    <h1>Sup?! I'm your friendly neighborhood Coop!</h1>
+    <p>What a world. That shit slap, don't it?</p>
+    <img src="https://source.unsplash.com/random/1200x600" alt="" />
+    
+    </div>
+            )
